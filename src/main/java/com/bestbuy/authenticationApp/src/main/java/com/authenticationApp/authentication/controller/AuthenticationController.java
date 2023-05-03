c26d8e9720f380f0a6e3c4660c77bc8b3561c67d
Ziaad-Khaled <ziadkhaled1918@gmail.com>
2023-05-03 17:11:50 +0300

    message queuing setup for authenication microservice is done

--- src/main/java/com/bestbuy/authenticationApp/src/main/java/com/authenticationApp/authentication/controller/AuthenticationController.java
+++ src/main/java/com/bestbuy/authenticationApp/src/main/java/com/authenticationApp/authentication/controller/AuthenticationController.java
@@ -3,6 +3,8 @@ package com.authenticationApp.authentication.controller;
 
 import com.authenticationApp.authentication.dao.AuthenticationDao;
 import com.authenticationApp.authentication.entity.AuthenticationEntity;
+import com.authenticationApp.authentication.queue.AuthenticationReceiver;
+import com.authenticationApp.authentication.queue.AuthenticationSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,10 @@ public class AuthenticationController {
 
     @Autowired
     private AuthenticationDao authenticationDao;
+    @Autowired
+    private AuthenticationSender authenticationSender;
+    @Autowired
+    private AuthenticationReceiver authenticationReceiver;
 
     @PostMapping(path = "/test")
     public AuthenticationEntity saveNewUser(
@@ -24,5 +30,11 @@ public class AuthenticationController {
     @GetMapping(path = "/getAllUsers")
     public List<AuthenticationEntity> getAllUsers() {
         return this.authenticationDao.getAllUsers();
+    }
+
+    @PostMapping(path = "/sendMessage")
+    public void sendMessage() {
+        System.out.println("test");
+        authenticationSender.sendMessage("test 2");
     }
 }
---
+++ src/main/java/com/bestbuy/authenticationApp/src/main/java/com/authenticationApp/authentication/queue/AuthenticationSender.java
@@ -0,0 +1,17 @@
+package com.authenticationApp.authentication.queue;
+
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationSender {
+    private final RabbitTemplate rabbitTemplate;
+    public AuthenticationSender(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend("AuthenticationSender", message);
+    }
+
+}
