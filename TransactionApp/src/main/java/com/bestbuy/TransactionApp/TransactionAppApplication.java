f3e70e402107d332b968fabdb885685d7fe1b2dd
Omar Khair <omarkhair66@gmail.com>
2023-05-04 15:55:17 +0300

    transactions app init

---
+++ TransactionApp/src/main/java/com/bestbuy/TransactionApp/TransactionAppApplication.java
@@ -0,0 +1,13 @@
+package com.bestbuy.TransactionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionAppApplication.class, args);
+	}
+
+}
---
+++ TransactionApp/src/main/java/com/bestbuy/TransactionApp/model/Stock.java
@@ -0,0 +1,11 @@
+package com.bestbuy.TransactionApp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Table(name = "stock")
+@Entity
+@Data
+public class Stock {
+}
