4ee8e1139ebc98fc42ef48ac2e6d2f37f5ed5f73
Hesham <hesham.moneer@outlook.com>
2023-05-05 21:30:02 +0300

    fixed shopping cart created at always null

--- TransactionApp/src/main/java/com/bestbuy/TransactionApp/TransactionAppApplication.java
+++ TransactionApp/src/main/java/com/bestbuy/TransactionApp/TransactionAppApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @OpenAPIDefinition(info=@Info(title="Transactions App API Docs"))
 public class TransactionAppApplication {
