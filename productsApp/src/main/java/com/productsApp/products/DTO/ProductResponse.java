2e8dcc87a8f877f629f59f55e3f9d1072da56185
Michael Girges <maicheltito1@gmail.com>
2023-05-09 13:36:57 +0300

    DTO on Product logic done, testing remains

---
+++ productsApp/src/main/java/com/productsApp/products/DTO/ProductResponse.java
@@ -0,0 +1,24 @@
+package com.productsApp.products.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+    @Id
+    private String id;
+    //    @Field(name="name")
+    private String productName;
+    //    @Field(name="price")
+    private BigDecimal productPrice;
+    //    @Field(name="description")
+    private String description;
+}
