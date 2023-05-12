8f0eca70e510917ab343c17ad844c8ba72b7385c
Ziaad-Khaled <77291238+Ziaad-Khaled@users.noreply.github.com>
2023-05-13 00:38:22 +0300

    Delete transactionApp directory

--- transactionApp/src/main/java/com/bestbuy/TransactionApp/model/PaymentMethod.java
+++
@@ -1,5 +0,0 @@
-package com.bestbuy.TransactionApp.model;
-
-public enum PaymentMethod {
-    CASH_ON_DELIVERY , CREDIT_CARD,INSTAPAY,VODAFONE_CASH
-}
--- transactionApp/src/main/java/com/bestbuy/TransactionApp/model/PaymentStatus.java
+++
@@ -1,5 +0,0 @@
-package com.bestbuy.TransactionApp.model;
-
-public enum PaymentStatus {
-    ACCEPTED,REJECTED
-}
