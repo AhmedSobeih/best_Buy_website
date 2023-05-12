e383d1e373099825316e3aa3b2c49cb9fca50d53
Hesham <hesham.moneer@outlook.com>
2023-05-12 04:34:16 +0300

    added logger configuration

--- TransactionApp/src/main/java/com/bestbuy/TransactionApp/controller/StockController.java
+++ TransactionApp/src/main/java/com/bestbuy/TransactionApp/controller/StockController.java
@@ -6,7 +6,8 @@ import com.bestbuy.TransactionApp.service.StockService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,40 +16,44 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/stock")
 @RequiredArgsConstructor
-@Slf4j
 @Tag(name = "Stock Controller")
 public class StockController {
     private final StockService stockService;
+    private final Logger logger = LoggerFactory.getLogger(StockController.class);
+
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<StockResponse> getAllStocks(){
-        log.info("Returned all stocks");
+        logger.info("Returned all stocks");
         return stockService.getAllStocks();
     }
 
     @GetMapping("/{product_id}")
     @ResponseStatus(HttpStatus.OK)
     public StockResponse getStockById(@PathVariable("product_id") String productId){
-        log.info("Sent stock of product with id: {}", productId);
+        logger.info("Sent stock of product with id: {}", productId);
         return stockService.getStockResponseById(productId);
     }
 
     @PutMapping ("/increment-stock")
     @ResponseStatus(HttpStatus.OK)
     public StockResponse incrementStockQuantity (@RequestBody  StockRequest stockRequest){
+        logger.info("Incremented Stock quantity --> product id: {}, added quantity: {}", stockRequest.getProductId(), stockRequest.getQuantity());
         return stockService.incrementStock(stockRequest.getProductId(),stockRequest.getQuantity());
     }
 
     @PutMapping ("/decrement-stock")
     @ResponseStatus(HttpStatus.OK)
     public StockResponse decrementStockQuantity (@RequestBody  StockRequest stockRequest){
+        logger.info("Decremented Stock quantity --> product id: {}, subtracted quantity: {}", stockRequest.getProductId(), stockRequest.getQuantity());
         return stockService.decrementStock(stockRequest.getProductId(),stockRequest.getQuantity());
     }
 
     @PutMapping ("/")
     @ResponseStatus(HttpStatus.OK)
     public StockResponse updateStock (@RequestBody  StockRequest stockRequest){
+        logger.info("Updated Stock: ", stockRequest);
         return stockService.updateStock(stockRequest);
     }
 
@@ -56,7 +61,7 @@ public class StockController {
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public StockResponse createStock(@RequestBody StockRequest stockRequest) {
-        log.info("Created stock: {}", stockRequest);
+        logger.info("Created stock: {}", stockRequest);
         return stockService.createStock(stockRequest.getProductId(), stockRequest.getPrice(), stockRequest.getQuantity());
     }
 }
