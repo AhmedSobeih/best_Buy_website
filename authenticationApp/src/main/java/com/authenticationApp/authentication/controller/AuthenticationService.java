cbf7f2c928257221d9e302c9305babfc036b5331
Ahmed Eid <64498799+AhmedSobeih@users.noreply.github.com>
2023-06-01 22:28:57 +0300

    Update AuthenticationService

--- authenticationApp/src/main/java/com/authenticationApp/authentication/controller/AuthenticationService.java
+++ authenticationApp/src/main/java/com/authenticationApp/authentication/controller/AuthenticationService.java
@@ -6,13 +6,16 @@ import com.authenticationApp.authentication.entity.AuthenticationEntity;
 import com.authenticationApp.authentication.entity.Role;
 import com.authenticationApp.authentication.repository.AuthenticationRepo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -23,39 +26,47 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
-
-    @Autowired
-    public RedisTemplate<String, String> redisTemplate;
+    private final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 
     public AuthenticationResponse register(RegisterRequest request) {
-        var user = AuthenticationEntity.builder()
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.USER)
-                .build();
-        var savedUser = repository.save(user);
+        //check if user exists
+        if (repository.findByEmail(request.getEmail()).isPresent()) {
+            logger.info("User already exists" );
+            return null;
+        }
+        else{
+            var user = AuthenticationEntity.builder()
+                    .email(request.getEmail())
+                    .password(passwordEncoder.encode(request.getPassword()))
+                    .role(Role.USER)
+                    .build();
+            var savedUser = repository.save(user);
+            logger.info("User registered" );
 
-        var jwtToken = jwtService.generateToken(user);
 
-        //var refreshToken = jwtService.generateRefreshToken(user);
-        //saveUserToken(savedUser, jwtToken);
-        return AuthenticationResponse.builder()
-                .token(jwtToken)
-                .build();
+            var jwtToken = jwtService.generateToken(user);
+            return AuthenticationResponse.builder()
+                    .token(jwtToken)
+                    .build();
+        }
+
     }
 
-    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+
+
+    public AuthenticationResponse login(AuthenticationRequest request) {
+        System.out.println("new password: "+request.getPassword());
+        System.out.println("email: "+request.getEmail());
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getEmail(),
                         request.getPassword()
                 )
+
         );
         var user = repository.findByEmail(request.getEmail())
                 .orElseThrow();
         var jwtToken = jwtService.generateToken(user);
-
-        redisTemplate.opsForValue().set(jwtToken, "myValue");
        // var refreshToken = jwtService.generateRefreshToken(user);
        // revokeAllUserTokens(user);
        // saveUserToken(user, jwtToken);
@@ -63,11 +74,57 @@ public class AuthenticationService {
                 .token(jwtToken)
                 .build();
     }
+    public AuthenticationResponse logout(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
 
-    public String getUserNameFromToken()
-    {
-        redisTemplate.opsForValue().set("zzz", "zzz");
-        return redisTemplate.opsForValue().get("zzz");
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        // var refreshToken = jwtService.generateRefreshToken(user);
+        // revokeAllUserTokens(user);
+        // saveUserToken(user, jwtToken);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
+    public String logout(String token) {
+
+        String filePath = "client.txt";
+        try {
+            FileWriter writer = new FileWriter(filePath);
+            writer.write("");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "logout successful";
+    }
+
+    public AuthenticationResponse changePassword(String mail, String newPassword) {
+        var user = repository.findByEmail(mail)
+                .orElseThrow();
+        user.setPassword(passwordEncoder.encode(newPassword));
+        repository.save(user);
+
+        // Re-authenticate the user with the updated credentials
+        System.out.println("new password: "+newPassword);
+        System.out.println("email: "+user.getEmail());
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getEmail(),
+                        newPassword
+                )
+        );
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
 }
