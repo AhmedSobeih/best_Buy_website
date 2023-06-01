3366364d326dbefcce3779d2f83d942ae834cb43
Mohamed Eshiba <mohamedeshiba08@gmail.com>
2023-06-01 16:22:59 +0300

    Docker images pushed to docker hub

--- run-apps.sh
+++ run-apps.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+echo "Logging in to Docker Hub..."
+docker login -u moeshiba -p Mohamed123
 apps=("authenticationApp" "messageApp" "productsApp" "TransactionApp" "productReviewsApp/reviews" "userApp")
+images=("authenticationapp-authentication_app" "messageapp-message_app" "productsapp-products_app" "transactionapp-transactions_app" "reviews-reviews_app" "userapp-user_app")
+
 
 pushd .
 
@@ -10,4 +13,10 @@ for app in "${apps[@]}"; do
     docker compose up --build -d
     popd
     pushd .
+done
+
+for image in "${images[@]}"; do
+    docker tag $image moeshiba/$image
+    docker repo create moeshiba/$image
+    docker push moeshiba/$image
 done
