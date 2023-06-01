831cdbe9ed112fe947fe30ff35eb7ea589495257
Ziaad-Khaled <ziadkhaled1918@gmail.com>
2023-06-01 22:07:36 +0300

    redis cache worked on authenticationApp

--- authenticationApp/src/main/java/com/authenticationApp/authentication/queue/AuthenticationReceiver.java
+++ authenticationApp/src/main/java/com/authenticationApp/authentication/queue/AuthenticationReceiver.java
@@ -1,10 +1,14 @@
 package com.authenticationApp.authentication.queue;
 
+import com.authenticationApp.authentication.command.AuthenticateCommand;
 import com.authenticationApp.authentication.command.Command;
 import com.authenticationApp.authentication.command.CommandFactory;
+import com.authenticationApp.authentication.controller.AuthenticationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.sound.midi.Receiver;
@@ -12,6 +16,11 @@ import javax.sound.midi.Receiver;
 @Component
 public class AuthenticationReceiver {
     private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+
+    @Autowired
+    AuthenticationService authenticationService;
+
     @RabbitListener(queues = "AuthenticationReceiver")
     public void receiveMessage(String message) {
         handleMessage(message);
@@ -19,9 +28,16 @@ public class AuthenticationReceiver {
         System.out.println(message);
     }
 
+    @RabbitListener(queues = "${rabbitmq.queues.auth.name}")
+    public String authenticate(String message){
+        return "User authenticated! with token "+ message;
+    }
+
+
+
     public void handleMessage(String message)
     {
-        Command command = CommandFactory.createCommand(message);
+        Command command = CommandFactory.createCommand(message, authenticationService);
         command.execute();
     }
 }
