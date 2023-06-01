835e13b354fc12a0905aab0cb35522a93208ce24
Ziaad-Khaled <ziadkhaled1918@gmail.com>
2023-06-01 19:01:03 +0300

    Connected transactionApp to AuthenticationApp and messages are sent and retrieved from the other app (using CommandPattern)

---
+++ TransactionApp/src/main/java/com/bestbuy/TransactionApp/controller/RabbitMQController.java
@@ -0,0 +1,24 @@
+package com.bestbuy.TransactionApp.controller;
+
+
+import com.bestbuy.TransactionApp.dto.OrderResponse;
+import com.bestbuy.TransactionApp.queue.AuthenticationSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/rabbit")
+public class RabbitMQController {
+
+    @Autowired
+    AuthenticationSender authenticationSender;
+
+    @GetMapping("/test")
+    public String test(){
+        authenticationSender.sendMessage("authenticate;token");
+        return "messageSent";
+    }
+}
