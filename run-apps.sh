c3fc743d20be56b8094c079d5c08f096980a0b04
Hesham <hesham.moneer@outlook.com>
2023-06-01 05:32:04 +0300

    added userApp to the script

--- run-apps.sh
+++ run-apps.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-apps=("authenticationApp" "messageApp" "productsApp" "TransactionApp" "productReviewsApp/reviews")
+apps=("authenticationApp" "messageApp" "productsApp" "TransactionApp" "productReviewsApp/reviews" "userApp")
+
+pushd .
 
 for app in "${apps[@]}"; do
     cd ./$app
     mvn clean package -DskipTests
     docker compose up --build -d
-    cd ../
+    popd
+    pushd .
 done
