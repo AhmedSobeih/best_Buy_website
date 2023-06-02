f5c2e2e07b09f546fb9a6b9872686828c0784bde
Mohamed Eshiba <mohamedeshiba08@gmail.com>
2023-06-02 23:06:06 +0300

    AutoScalling

--- run-apps.sh
+++ run-apps.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 echo "Logging in to Docker Hub..."
-#docker login -u moeshiba -p Mohamed123
-apps=("authenticationApp" "messageApp" "productsApp" "TransactionApp" "productReviewsApp/reviews" "userApp")
-images=("authenticationapp-authentication_app" "messageapp-message_app" "productsapp-products_app" "transactionapp-transactions_app" "reviews-reviews_app" "userapp-user_app")
+docker login -u moeshiba -p Mohamed123
+apps=("authenticationApp" )
+images=("authenticationapp-authentication_app")
+docker compose up --build -d #builds the container of rabbitmq server
 
-#docker compose up --build -d #builds the container of rabbitmq server
-#
-#pushd .
-#
-#for app in "${apps[@]}"; do
-#    cd ./$app
-#    mvn clean package -DskipTests
-#    docker compose up --build -d
-#    popd
-#    pushd .
-#done
+pushd .
+
+for app in "${apps[@]}"; do
+    cd ./$app
+    mvn clean package -DskipTests
+    docker compose up --build -d
+    popd
+    pushd .
+done
 
 for image in "${images[@]}"; do
     docker tag $image moeshiba/$image
